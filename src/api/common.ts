da8dbfb52da3775523500a33767cedb5a4da5fcd
houbinghui <houbinghuia@163.com>
2023-05-10 18:01:52 +0800

    初始化

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,18 +1,14 @@
-import request from '@/utils/request'
-import {Data} from '@/types/common' 
+import request from "@/utils/request"
 
 // get请求
-function dict(url:string,params?:Data) {
-   return request({url,method:'get',params})
+function dict(url: string, params: any) {
+  return request({ url, method: "get", params })
 }
 
 // post请求
-function dict2(url:string,data?:Data) {
-   return request({url,method:'post',data})
+function dict2(url: string, data: any) {
+  return request({ url, method: "post", data })
 }
 
 // 统一导出
-export {
-   dict,
-   dict2
-}
+export { dict, dict2 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,42 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 // 配置ref，reactive等的自动导入
-import AutoImport from 'unplugin-auto-import/vite'
+import AutoImport from "unplugin-auto-import/vite"
 // 按需引入组件
-import Components from 'unplugin-vue-components/vite';
+import Components from "unplugin-vue-components/vite"
 // 需要按需引入那个组件库,就用去源码看哪个，以前缀为开头
-import { VantResolver } from 'unplugin-vue-components/resolvers'
+import { VantResolver } from "unplugin-vue-components/resolvers"
 
-const path = require('path')
+const path = require("path")
 
 export default defineConfig({
-  plugins: [vue(),AutoImport({
-    dts: 'src/auto-import.d.ts',
-    imports: ['vue', "vue-router"],
-  }), Components({
-     // 默认就是识别src/components文件，该文件夹下的所有组件都会自动 import，配合setup语法糖直接使用
-    dirs: ['src/components'],
-     resolvers: [
-          VantResolver()
-      ],
-  })],
-   resolve: {
-     alias: {
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: "src/types/auto-import.d.ts",
+      imports: ["vue", "vue-router"],
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      // 默认就是识别src/components文件，该文件夹下的所有组件都会自动 import，配合setup语法糖直接使用
+      dirs: ["src/components"],
+      dts: "src/types/components.d.ts",
+      resolvers: [VantResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
       // 配置此项，可以ctrl+左键跳转到使用@开头的源文件，不加无法跳转
-      '@': path.resolve(__dirname, './src')
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   server: {
     proxy: {
-      '/fmsv2': {
+      "/fmsv2": {
         target: "http://192.168.180.138:8081/",
-        changeOrigin:true,
+        changeOrigin: true,
         // rewrite: (path)=> path.replace('^\/fmsv2/','')
-      }
-    }
-  }
+      },
+    },
+  },
 })
